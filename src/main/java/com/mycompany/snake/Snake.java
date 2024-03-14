680e9e91847f1fe74b0f2450f01782cfd45e6cd2
alu11563090 <alu11563090@ti2-01>
2024-03-14 20:12:50 +0100

    Initial commit

---
+++ src/main/java/com/mycompany/snake/Snake.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.snake;
+
+/**
+ *
+ * @author alu11563090
+ */
+public class Snake {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
